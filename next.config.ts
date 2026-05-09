eec3e7d343f4dc132aee16ac7f994124e46a3836
Talhaahmad9 <talhaahmaed999@gmail.com>
2026-05-09 10:22:43 +0500

    feat: edited next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
 import type { NextConfig } from "next";
 
-const r2Hostname = process.env.NEXT_PUBLIC_R2_PUBLIC_URL 
-  ? new URL(process.env.NEXT_PUBLIC_R2_PUBLIC_URL).hostname 
+const r2Hostname = process.env.NEXT_PUBLIC_R2_PUBLIC_URL
+  ? new URL(process.env.NEXT_PUBLIC_R2_PUBLIC_URL).hostname
   : "*.r2.dev";
 
 const nextConfig: NextConfig = {
@@ -12,6 +12,22 @@ const nextConfig: NextConfig = {
         hostname: r2Hostname,
       },
     ],
+  },
+
+  async redirects() {
+    return [
+      {
+        source: "/:path*",
+        has: [
+          {
+            type: "host",
+            value: "talhaahmad.vercel.app",
+          },
+        ],
+        destination: "https://talhaahmad.me/:path*",
+        permanent: true,
+      },
+    ];
   },
 };
 
